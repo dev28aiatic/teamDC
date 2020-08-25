569686b3a724cf741ac9c9a95a0c3f583ea14c64
Ronal <dev24@aiatic.com>
2020-08-25 08:12:18 -0500

    Avances en la implementación del service el componente de register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
 
 //Importar servicio
-import {RegistrosService} from 'src/app/services/registros.service';
+import { RegistrosService } from 'src/app/services/registros.service';
+import { Observable } from 'rxjs';
+import { RegistrosI } from 'src/app/models/registros.interface';
 //inyectar service
 //usar service
 
@@ -14,28 +16,65 @@ import {RegistrosService} from 'src/app/services/registros.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
 
-
-
-
-  //Control del email 
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  errorEmail() {
-    if (this.email.hasError('required')) {
-      return 'Debe ingresar un email';
-    }
-
-    return this.email.hasError('email') ? 'Email no válido' : '';
-  }
+export class RegisterComponent  implements OnInit {
 
   
   //inyectar service
-  constructor( private registrosService:RegistrosService) { }
+  constructor( private registrosService : RegistrosService) { 
+
+  }
 
   ngOnInit(): void {
     this.registrosService.getRegistros().subscribe(res => console.log(res));
   }
+
+  //Control del email 
+  emails = new FormControl('', [Validators.required, Validators.email]);
+
+  errorEmail() {
+    if (this.emails.hasError('required')) {
+      return 'Debe ingresar un email';
+    }
+    this.rg.email='ronal@gmail.com'
+    return this.emails.hasError('email') ? 'Email no válido' : '';
+  }
+
+
+   //devlarar una nueva "clase" u Objeto de la interface
+    rg: RegistrosI = { 
+    
+    id:'dvzxvx',
+    nombres: 'dvszvx',
+    apellidos: 'cxvxv',
+    cedula: 2432554,
+    email: 'dfszdsf',
+    fechaNacimiento:'sfsdzfsz',
+    direccion: 'szfdzsfd',
+    ciudad: 'zfdszdf',
+    departamento:'sfdszf',
+    pais: 'zdfzsf',
+    codigoPostal:'zdfszf',
+    profesion: 'szfdsz',
+    habilidades:'sfds',
+    descripcion: 'zfdsfds',
+  }
+
+  
+
+
+
+
+  onRegister()
+  {
+    
+    console.log(this.rg)
+
+    this.registrosService.addRegistro(this.rg)
+           
+     
+  }
+
+  
 
 }
