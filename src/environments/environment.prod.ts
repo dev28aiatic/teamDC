3728e3f9159a8d721691a1d487a3e0dbe676f75e
Karen Gómez <dev28@aiatic.com>
2020-08-27 07:39:57 -0500

    configuracion environment y detalles de estilo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebaseConfig: {
+    apiKey: "AIzaSyDMgm8P30Sp2NVZG9tf46BcONGHQ3Z2n0k",
+    authDomain: "teamdc-c1083.firebaseapp.com",
+    databaseURL: "https://teamdc-c1083.firebaseio.com",
+    projectId: "teamdc-c1083",
+    storageBucket: "teamdc-c1083.appspot.com",
+    messagingSenderId: "487516474484",
+    appId: "1:487516474484:web:3514ec6d1e078126195521"
+  }
 
 
 };
