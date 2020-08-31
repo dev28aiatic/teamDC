6cb7832632c7e20816ffdb53f6d9573789d8567f
Karen Gómez <dev28@aiatic.com>
2020-08-31 17:12:59 -0500

    login-restricciones

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
+  providers:[AuthService]
 })
 export class SidenavComponent {
 
@@ -16,6 +21,19 @@ export class SidenavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    public user$: Observable<any> = this.authSvc.afAuth.user;
+
+  constructor(private breakpointObserver: BreakpointObserver, private authSvc: AuthService,
+    private router: Router,) {}
+
+  async onLogout(){
+    try{
+      await this.authSvc.logout();
+      this.router.navigate(['/login']);
+    }
+    catch(error){console.log(error)}
+  
+  
+  }
 
 }
