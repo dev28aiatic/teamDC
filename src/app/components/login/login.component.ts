c2037dd1084c5cdf3bfd45b741c35adda06b3552
Karen Gómez <dev28@aiatic.com>
2020-08-31 16:02:39 -0500

    login-no valida

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { auth } from 'firebase';
+import { AuthService } from '../../services/auth.service';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  providers:[AuthService],
+})
+export class LoginComponent implements OnInit {
+
+ 
+
+  loginForm= new FormGroup({
+    
+    email: new FormControl('',[Validators.required, Validators.email]),
+    
+  })
+
+  constructor(private authSvc:AuthService, private router: Router) { }
+
+  ngOnInit(): void {}
+
+  async onLogin(){
+    const { email,password } = this.loginForm.value;
+    try{      
+     const user= await this.authSvc.login(email,password);
+     if (user) {
+       //redirect to homePage
+       this.router.navigate(['/home'])
+     }
+
+    }catch(error){console.log(error)};
+    
+
+    }
+    
+    get email(){ return this.loginForm.get('email');}
+    get password(){ return this.loginForm.get('password');}
+
+  
+
+  
+  
+
+  }
