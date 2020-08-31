e535192113a1d96c608cd5df93344dcb27b0332a
Karen Gómez <dev28@aiatic.com>
2020-08-31 16:41:55 -0500

    login-email-password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -19,6 +19,7 @@ export class LoginComponent implements OnInit {
   loginForm= new FormGroup({
     
     email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required])
     
   })
 
@@ -30,6 +31,7 @@ export class LoginComponent implements OnInit {
     const { email,password } = this.loginForm.value;
     try{      
      const user= await this.authSvc.login(email,password);
+     console.log(email,password);
      if (user) {
        //redirect to homePage
        this.router.navigate(['/home'])
@@ -41,11 +43,5 @@ export class LoginComponent implements OnInit {
     }
     
     get email(){ return this.loginForm.get('email');}
-    get password(){ return this.loginForm.get('password');}
-
-  
-
-  
-  
-
+    
   }
