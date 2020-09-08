c51d5cf37c27d352529d6e3c015789d52dcc142e
Ronal <dev24@aiatic.com>
2020-09-08 17:23:50 -0500

    se obtienen los datos de la bd y se muestra en mi perfil

--- src/app/components/profile/profile-routing.module.ts
+++ src/app/components/profile/profile-routing.module.ts
@@ -3,10 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from './profile.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 const routes: Routes = [{ path: '', component: ProfileComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
+  
+  
+  ],
+  exports: [RouterModule,
+    FormsModule,
+    ReactiveFormsModule]
 })
 export class ProfileRoutingModule { }
