3838647b3179acb3bd5bab796cc3ebcff62a8919
Ronal <dev24@aiatic.com>
2020-09-14 13:01:21 -0500

    Ya se guarda los datos de contacto

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import {ContactosService} from 'src/app/services/contactos.service'
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -11,9 +13,20 @@ export class ContactComponent implements OnInit {
   //para el formGroup
   contactForm: FormGroup;
 
+  listaContactos;
+
+  // para el selecionar le motivo
+  motivos= [
+    {value: 'Contratos' },
+    {value: 'Proyectos'},
+    {value: 'Cobranzas'}
+  ];
+  motivoSeleccionado:string;
+
   constructor(
     //para el formulario
-    private fb:FormBuilder
+    private fb:FormBuilder,
+    private contactosService:ContactosService,
   ) {
 
     this.contactForm= this.fb.group({
@@ -24,6 +37,8 @@ export class ContactComponent implements OnInit {
       mensaje: new FormControl('', [Validators.required]),
 
     });
+
+    
 
    }
 
@@ -37,10 +52,55 @@ export class ContactComponent implements OnInit {
       mensaje: ''
 
     });
+    this.getRegistros();
+  }
 
+  getRegistros(){
+
+    this.contactosService.getContactos().subscribe((rgSnapshot) => {
+      this.listaContactos = [];
+      rgSnapshot.forEach((rgData: any) => {
+        this.listaContactos.push({
+          id: rgData.payload.doc.id,
+          data: rgData.payload.doc.data()
+
+        });
+      })
+
+      console.log(this.listaContactos);
+      
+
+    });
+
+    
+  }
+
+  //metodo para informar errores en el campo de email
+  errorEmail() {
+    if (this.contactForm.controls.email.hasError('required')) {
+      return 'Debe ingresar un email';
+    }
+    if (this.contactForm.controls.email.hasError('ms')) {
+      return 'El email ya ha sido registrado';
+    }
+    return this.contactForm.controls.email.hasError('email') ? 'Email no válido' : '';
   }
 
   oncreate(form){
 
+    this.contactosService.crearContacto(this.contactForm.value).then(() => {
+
+      this.contactForm.setValue({
+
+        nombreCompleto: '', 
+        email: '', 
+        motivo: '',
+        mensaje: ''
+  
+      });
+      
+    }, (error) => {
+      console.error(error);
+    }); 
   }
 }
