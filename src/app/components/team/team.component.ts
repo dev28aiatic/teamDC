3e5ed42c2e0bf4ba1b2280fc368ea78d030a7b53
Karen Gómez <dev28@aiatic.com>
2020-09-15 14:34:49 -0500

    equipo

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-team',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit {
+  public isMobile: boolean = false;
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.Handset
+    ]).subscribe(result => {
+      this.isMobile = result.matches;
+    });
+  }
 
-  constructor() { }
 
   ngOnInit(): void {
   }
